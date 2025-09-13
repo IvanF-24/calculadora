b6c2207c5aebcce78748fe8a89b0b47368869a51
IvanF-24 <ivanflorez134@gmail.com>
2025-09-13 18:50:20 -0500

    Funcion factorial agregada

--- index.js
+++ index.js
@@ -48,6 +48,21 @@ function dividir(num1, num2) {
   return num1 / num2;
 }
 
+// Función para calcular el factorial de un número
+function factorial(num) {
+  if (num < 0) {
+    return "Error: No existe el factorial de un número negativo";
+  }
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let resultado = 1;
+  for (let i = 2; i <= num; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
