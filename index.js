9df9a4e7744fba5477d37691f5e51399a9dcc9a5
IvanF-24 <ivanflorez134@gmail.com>
2025-09-13 18:38:26 -0500

    funcion multiplicar

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ function restar(num1, num2) {
   return num1 - num2;
 }
 
+// Función para multiplicar dos números
+function multiplicar(num1, num2) {
+  return num1 * num2;
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
